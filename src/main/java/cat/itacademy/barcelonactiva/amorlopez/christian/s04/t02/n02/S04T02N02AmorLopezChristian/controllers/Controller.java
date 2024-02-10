758444fb2561bce1719397777a511525d6b108b4
Christian Amor Lopez <151139448+christianamor3@users.noreply.github.com>
2024-02-10 17:54:07 +0100

    Introduccion de ResponseEntity<>

--- src/main/java/cat/itacademy/barcelonactiva/amorlopez/christian/s04/t02/n02/S04T02N02AmorLopezChristian/controllers/Controller.java
+++ src/main/java/cat/itacademy/barcelonactiva/amorlopez/christian/s04/t02/n02/S04T02N02AmorLopezChristian/controllers/Controller.java
@@ -24,30 +24,50 @@ public class Controller {
 	FruitaEntityServiceImp fruitaEntityServiceImp;
 	
 	@PostMapping("/add")
-	public FruitaEntity addFruita(@RequestBody FruitaEntity fruita) {
-		fruitaEntityServiceImp.save(fruita);
-		return fruita;
+	public ResponseEntity<FruitaEntity> createFruitaEntity(@RequestBody FruitaEntity fruita) {
+		FruitaEntity fruitaEntity = fruitaEntityServiceImp.save(fruita);
+		return ResponseEntity<>(fruitaEntity, HttpStatus.CREATED);
 	}
 	
 	@PutMapping("/update/{id}")
-	public FruitaEntity updateFruitaEntity(@PathVariable int id, @RequestBody FruitaEntity fruita) {
-		fruitaEntityServiceImp.update(id, fruita);
-		return fruita;
+	public ResponseEntity<FruitaEntity> updateFruitaEntity(@PathVariable int id, @RequestBody FruitaEntity fruita) {
+		FruitaEntity fruitaEntity = fruitaEntityServiceImp.update(id, fruita);
+		
+		if (fruitaEntity != null){
+			return ResponseEntity<>(fruitaEntity, HttpStatus.OK);
+		} else {
+			return ResponseEntity<>(fruitaEntity, HttpStatus.NOT_FOUND);
+		}
 	}
 	
 	@DeleteMapping("/delete/{id}")
-	public String deleteFruitaEntity (@PathVariable int id) {
-		return fruitaEntityServiceImp.delete(id);
+	public ResponseEntity<String> deleteFruitaEntity (@PathVariable int id) {
+		fruitaEntityServiceImp.delete(id);
+		return ResponseEntity<>(HttpStatus.NO_CONTENT);	
 	}
 	
 	@GetMapping("/getAll")
-	public List<FruitaEntity> getAllFruitaEntity() {
-		return fruitaEntityServiceImp.findAll();
+	public ResponseEntity<List<FruitaEntity>> getAllFruitaEntity() {
+		List<FruitaEntity> fruites = fruitaEntityServiceImp.findAll();
+		
+		if (fruites.isEmpty()){
+			return ResponseEntity<>(HttpStatus.NOT_FOUND);
+		} else {
+			return ResponseEntity<>(fruites, HttpStatus.OK);	
+		}
 	}
 	
 	@GetMapping("/getOne/{id}")
-	public FruitaEntity getOneFruitaEntity (@PathVariable int id){
-		return fruitaEntityServiceImp.findById(id);
+	public ResponseEntity<FruitaEntity> getOneFruitaEntity (@PathVariable int id){
+		FruitaEntity fruitaEntity = fruitaEntityServiceImp.findById(id);
+		
+		if (fruitaEntity != null){
+			return ResponseEntity<>(fruitaEntity, HttpStatus.OK);
+		} else {
+			return ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+
 	}
 	
 
